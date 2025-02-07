4445b521c63ff08d4000905e61a3c0e8d7318ac9
DeepBlue <126600616+DeepBlue-dot@users.noreply.github.com>
2025-02-07 17:47:09 +0300

    Update userRoutes.js

--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -9,12 +9,13 @@ import {
 } from "../controllers/UserController.js";
 import asyncHandler from "../utils/asyncHandler.js";
 import authenticateUser from "../middleware/authenticateUser.js";
+import upload from "../config/multerConfig.js";
 
 const userRoutes = express.Router();
 
-userRoutes.route("/").post(asyncHandler(userRegister)).get(asyncHandler(getAllUsers));
+userRoutes.route("/").post(upload.single("profilePicture"), asyncHandler(userRegister)).get(asyncHandler(getAllUsers));
 userRoutes.use("/me", asyncHandler(authenticateUser));
-userRoutes.route("/me").get(asyncHandler(getUser)).patch(asyncHandler(updateUser)).delete(asyncHandler(deleteUser));
+userRoutes.route("/me").get(asyncHandler(getUser)).patch(upload.single("profilePicture"), asyncHandler(updateUser)).delete(asyncHandler(deleteUser));
 
 userRoutes.route("/:id").get(asyncHandler(getUserById));
 
