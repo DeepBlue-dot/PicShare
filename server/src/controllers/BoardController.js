cdeb57579640ab4d0edb23e061a08691f2f775fc
DeepBlue <126600616+DeepBlue-dot@users.noreply.github.com>
2025-02-08 23:19:27 +0300

    updated board controller and route

---
+++ server/src/controllers/BoardController.js
@@ -0,0 +1,23 @@
+export async function createBoard(req, res) {}
+
+export async function getAllBoard(req, res) {}
+
+export async function getBoardById(req, res) {}
+
+export async function updateBoard(req, res) {}
+
+export async function deleteBoard(req, res) {}
+
+export async function addPost(req, res) {}
+
+export async function deletePost(req, res) {}
+
+export async function getAllPosts(req, res) {}
+
+export async function searchBoards(req, res) {}
+
+export async function getBoardsbyUser(req, res) {}
+
+export async function getUserBoards(req, res) {}
+
+export async function addTags(req, res) {}
--- server/src/routes/boardRoutes.js
+++ server/src/routes/boardRoutes.js
@@ -1,6 +1,40 @@
 import express from "express";
+import {
+  createBoard,
+  getAllBoard,
+  getBoardById,
+  updateBoard,
+  deleteBoard,
+  addPost,
+  deletePost,
+  getAllPosts,
+  searchBoards,
+  getBoardsbyUser,
+  getUserBoards,
+  addTags,
+} from "../controllers/BoardController.js";
+const boardRoutes = express.Router();
 
-const boardRoutes = express.Router()
+// Board CRUD routes
+boardRoutes.route("/").post(createBoard).get(getAllBoard);
 
+boardRoutes
+  .route("/:boardId")
+  .get(getBoardById)
+  .patch(updateBoard)
+  .delete(deleteBoard);
 
-export default boardRoutes
+// Post-related routes
+boardRoutes.route("/:boardId/posts").post(addPost).get(getAllPosts);
+
+boardRoutes.route("/:boardId/posts/:postId").delete(deletePost);
+
+// Search and user-specific routes
+boardRoutes.route("/search").get(searchBoards);
+boardRoutes.route("/user/:userId").get(getBoardsbyUser);
+boardRoutes.route("/me/boards").get(getUserBoards);
+
+// Tag-related routes
+boardRoutes.route("/:boardId/tags").patch(addTags);
+
+export default boardRoutes;
